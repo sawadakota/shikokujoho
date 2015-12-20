68240681d794944c992387c03d71ed3490d42281
澤田皇汰 <sawadakota@ksawada.local>
2015-12-20 13:42:46 +0900

    like sum displayed every tag

--- app/controllers/article/tags_controller.rb
+++ app/controllers/article/tags_controller.rb
@@ -6,5 +6,14 @@ class Article::TagsController < ApplicationController
   def show
     @models = Model.tagged_with(params[:id])
     @tag = params[:id]
+    @tags = Model.tagged_with("#{@tag}")
+    @sum = 0
+    @likes = @tags.each do |tag|
+      @sum += tag.likes_count
+    end
+    @ehime_models = Model.tagged_with("愛媛県")
+    @kochi_models = Model.tagged_with("高知県")
+    @tokushima_models = Model.tagged_with("徳島県")
+    @kagawa_models = Model.tagged_with("香川県")
   end
 end
