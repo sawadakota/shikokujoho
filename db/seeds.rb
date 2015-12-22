2e772a2775ee1210a1a2674cc4f27d90e55f1800
澤田皇汰 <sawadakota@ksawada.local>
2015-12-22 17:01:59 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+require "csv"
+
+companies_csv = CSV.readlines("db/models.csv")
+companies_csv.shift
+companies_csv.each do |row|
+  Model.create(name: row[1], college: row[2], profile: row[3], created_at: row[4], updated_at: row[5], furigana: row[6], likes_count: row[7], birthday: row[8], college2: row[9], lovetype: row[10], highschool: row[11], charmpoint: row[12], personality: row[13])
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
