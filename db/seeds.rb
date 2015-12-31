31687ce13b980d645ffce795de80173dca5e501c
澤田皇汰 <sawadakota@ksawada.local>
2015-12-31 19:31:01 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 require "csv"
 
-companies_csv = CSV.readlines("db/models.csv")
-companies_csv.shift
-companies_csv.each do |row|
+models_csv = CSV.readlines("db/models.csv")
+models_csv.shift
+models_csv.each do |row|
   Model.create(name: row[1], college: row[2], profile: row[3], created_at: row[4], updated_at: row[5], furigana: row[6], likes_count: row[7], birthday: row[8], college2: row[9], lovetype: row[10], highschool: row[11], charmpoint: row[12], personality: row[13])
 end
 
